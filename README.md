ba3fb8bc446a101a3e182b430a486570671099ab
tianxintian22 <1247016369@qq.com>
2017-06-18 22:43:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Java web demo 
+Use maven spring mvc
---
+++ src/main/java/com/test/webdemo/controller/HelloMvcController.java
@@ -0,0 +1,14 @@
+package com.test.webdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloMvcController {
+	
+	@RequestMapping("/mvc")
+	public String helloMvc() {
+		return "home";
+	}
+}
